6357a43ef70e69b1ba4a2c57c92b3290cc1b903f
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-05-21 00:39:20 +0200

    Miletone 5 , manca qualche dettaglio grafico

--- Milestone_5/assets/js/main.js
+++ Milestone_5/assets/js/main.js
@@ -4,6 +4,7 @@ const app = new Vue({
         counter: 0,
         message: "",
         search: "",
+        prova: "none",
         contacts: [
             {
                 name: 'Michele',
@@ -13,17 +14,20 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        visibility: "none"
                     }
                 ],
             },
@@ -35,17 +39,20 @@ const app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        visibility: "none"
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     }
                 ],
             },
@@ -58,17 +65,20 @@ const app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        visibility: "none"
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        visibility: "none"
                     }
                 ],
             },
@@ -80,12 +90,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        visibility: "none"
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        visibility: "none"
                     }
                 ],
             },
@@ -99,6 +111,7 @@ const app = new Vue({
             newMessage.text = this.message;
             newMessage.date = dayjs().format("DD/MM/YYYY  HH:mm:ss");
             newMessage.status = "sent";
+            newMessage.visibility = "none";
 
             //console.log(newMessage);
             this.contacts[this.counter].messages.push(newMessage);
@@ -108,11 +121,31 @@ const app = new Vue({
                 automaticMessage = {
                     text: "Solo io ti rispondo subito",
                     date: dayjs().format("DD/MM/YYYY  HH:mm:ss"),
-                    status: "received"
+                    status: "received",
+                    visibility: "none"
                 };
                 this.contacts[this.counter].messages.push(automaticMessage);
 
             }, 2000)
+        },
+
+        //funzione che rimuove il messagio selezionato
+        removeMessage(index) {
+            let conferma = confirm("Sei sicuro di voler cancellare il messaggio?");
+            if (conferma) {
+                this.contacts[this.counter].messages.splice(index, 1);
+            } else {
+                this.contacts[this.counter].messages[index].visibility = "none";
+
+            }
+        },
+
+        showInfo(index) {
+            if (this.contacts[this.counter].messages[index].visibility === "none") {
+                this.contacts[this.counter].messages[index].visibility = "block";
+            } else {
+                this.contacts[this.counter].messages[index].visibility = "none";
+            }
         },
 
 
